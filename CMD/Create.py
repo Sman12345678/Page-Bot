db5806ac653e9431f3e457a66876b26f8aa60d4a
Suleiman <Suleimanayomide716@gmail.com>
2025-01-18 17:57:37 +0100

    Update Create.py

--- CMD/Create.py
+++ CMD/Create.py
@@ -11,7 +11,7 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s"
 )
-
+sender_id=" "
 def execute(message):
     """
     Scrapes an image from Bing based on the search term and uploads it to Facebook.
