7a74fd00015295e159bc1f720e2df00ecdba066e
Suleiman <Suleimanayomide716@gmail.com>
2025-01-21 01:10:26 +0100

    Update search.py

--- CMD/search.py
+++ CMD/search.py
@@ -1,6 +1,8 @@
 # Brain.py This File is Designed For The Purpose Of Making Bot Wiser Please Do Not Misuse or Abuse API Key
 import requests
-
+Info={
+    "Description":"Perform Google Search"
+}
 def execute(message):
     """
     Perform a Google search using the provided message text 
