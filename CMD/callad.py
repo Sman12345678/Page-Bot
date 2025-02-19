6866c30d0db9845a732a740906cca064d680496e
Suleiman <Suleimanayomide716@gmail.com>
2025-02-19 15:43:25 +0100

    Update callad.py

--- CMD/callad.py
+++ CMD/callad.py
@@ -1,11 +1,22 @@
-
 import app as Suleiman
 
 user_id = 8711876652167640  
 
 def execute(message):
-    if message is None: 
-        return "🧘 Please provide a message to be sent to Admin"
-    else:
-        response = Suleiman.send_message(user_id, message)
-        return response
+    if not message:  # More Pythonic way to check for None or empty string
+        return Suleiman.send_message(user_id, "🧘 Please provide a message to be sent to Admin")
+
+    response = Suleiman.send_message(user_id, 
+        f"""📩 |====== Quick Message ======|
+
+👨‍💻 **Message From**: `{user_id}`  
+
+📜 |====== Body ======|  
+{message}  
+
+📬 |===================|"""
+    )
+
+    Suleiman.send_message(user_id, "✅ Message sent successfully")
+    return response
+
