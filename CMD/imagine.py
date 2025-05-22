68e13f666e9e1f5a2c4eb66e593de9f2fecae1fc
Suleiman <Suleimanayomide716@gmail.com>
2025-05-22 16:02:57 +0100

    Update imagine.py

--- CMD/imagine.py
+++ CMD/imagine.py
@@ -24,7 +24,7 @@ def execute(message, sender_id):
                 }
             ]
         data = response.json()
-        image_url = data.get("image")
+        image_url = data.get("images")
         text_response = data.get("response", "")
 
         messages = []
